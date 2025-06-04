a8573e9299d005a1eda132e6dc59545cfee6a79c
Riyaz-Aazu <riyazgummepalli@gmail.com>
2025-06-04 14:23:22 +0530

    Add files via upload

---
+++ lineasearch.java
@@ -0,0 +1,24 @@
+package javaprograms;
+import java.util.Scanner;
+
+public class lineasearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-gene,,,rated method stub
+		int[] numbers= {5,6,7,9,2,};
+		int target =9;
+		boolean found=false;
+		for(int i=0; i<numbers.length;i++){
+			if(numbers[i]==target) {
+				System.out.println("Found"+" "+target+" at index"+" "+ i);
+				found=true;
+				break;
+			}
+		}
+		if(!found) {
+			System.out.println("Number not found");
+		}
+
+	}
+
+}
